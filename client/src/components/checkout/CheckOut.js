6d0febced88f2e78aeb2d5169fe62b5ea4fbd0ed
harishkumarpendem@gmail.com <harishkumarpendem@gmail.com>
2021-04-02 10:51:41 +0530

    hello

--- client/src/components/checkout/CheckOut.js
+++ client/src/components/checkout/CheckOut.js
@@ -3,7 +3,7 @@ import React,{Fragment} from 'react'
 let CheckOut = () => {
     return (
         <Fragment>
-         <h1>checkout page</h1>
+         <h2>checkout page</h2>
         </Fragment>
     );
 };
